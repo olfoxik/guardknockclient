e21aae21b9c2a5f0776eadfbe75d0b714b0f4965
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-17 22:32:54 +0300

    Update guardknockclient.go

--- guardknockclient.go
+++ guardknockclient.go
@@ -18,7 +18,7 @@ func main() {
  
     // Замените URL на целевой REST API
     //url := "http://95.140.159.65:3004/"
-    url := cfg.Section("Main").Key("url).String()
+      url := cfg.Section("Main").Key("url).String()
     // Замените вашими учетными данными
  //   username := "olfox2"
   //  password := "tuxpux7"
